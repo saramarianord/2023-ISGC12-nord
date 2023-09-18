67e2840f9b52972fbefd4b93493656ef92257d21
vespertria <saranordd@hotmail.com>
2023-09-18 10:32:58 +0200

    uppd

--- Lab2/app/src/main/java/com/example/lab2/Activity2.java
+++ Lab2/app/src/main/java/com/example/lab2/Activity2.java
@@ -11,12 +11,13 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class Activity2 extends AppCompatActivity {
-    String ord[] = {"banan", "hejhejhejhej", "teater"};
+    private String ord[] = {"banan", "hejhejhejhej", "teater"};
     Random random = new Random();
-    int forsok = 7;
-    String slumpatOrd;
-    StringBuilder doltOrd;
-    TextView slumpadTextView; // Deklarera en TextView-variabel
+    private int forsok = 7;
+   private String slumpatOrd;
+   private StringBuilder doltOrd;
+   private TextView slumpadTextView;
+    private TextView antForsok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,27 +26,26 @@ public class Activity2 extends AppCompatActivity {
 
         slumpadTextView = findViewById(R.id.slumpad);
         slumpatOrd = ord[random.nextInt(ord.length)];
-
+        antForsok = findViewById(R.id.antForsok);
         doltOrd = new StringBuilder();
+
         for (int i = 0; i < slumpatOrd.length(); i++) {
             doltOrd.append("_");
         }
-
         slumpadTextView.setText(doltOrd.toString());
     }
 
     public void guessLetter(View view) {
         EditText guessEditText = findViewById(R.id.guess);
-        String input = guessEditText.getText().toString(); // Gör inmatningen till små bokstäver
+        String input = guessEditText.getText().toString();
 
         for (int i = 0; i < slumpatOrd.length(); i++) {
             if (slumpatOrd.charAt(i) == input.charAt(0)) {
                 doltOrd.setCharAt(i, input.charAt(0));
             }
         }
-
-        slumpadTextView.setText(doltOrd.toString());
-        guessEditText.setText("");
+            slumpadTextView.setText(doltOrd.toString());
+            guessEditText.setText("");
 
         if(!slumpadTextView.getText().toString().contains("_")){ // https://stackoverflow.com/questions/9915203/how-to-check-if-a-textview-contains-a-certain-string
 
